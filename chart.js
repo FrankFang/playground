35bf49197a7a89aea0faf34d7046a03edbb70413
frankfang <frankfang1990@gmail.com>
2015-02-11 16:38:11 +0800

    IE 下每次加入 250 个点是可以接受的，低于2秒。Chrome下加1000个点大概1秒。

--- chart.js
+++ chart.js
@@ -10,7 +10,7 @@ define(['require'], function (require) {
     }
 
     function generatePoint() {
-        return {x: generateInt(800), y: generateInt(600)}
+        return {x: generateInt(800), y: generateInt(400)}
     }
 
     function generateInt(min, max) {
@@ -25,32 +25,58 @@ define(['require'], function (require) {
         return Math.round(Math.random() * (max - min)) + min
     }
 
-    var dataset = generatePoints(5000)
+    var dataset = generatePoints(250)
 
-    var dataset2 = [5, 10, 15, 20, 25, 40, 45, 50, 55, 60]
+    var dataset2 = generatePoints(1000)
 
 
+    var count = 1
     var svg = d3.select('body').append('svg')
         .attr('width', 800)
-        .attr('height', 600)
+        .attr('height', 400)
         .on('click', function () {
-            update(dataset2)
+            var start = new Date()
+            //for (var i = 0; i < count; i++) {
+            //    for (var j = 0; j < 500; j++) {
+            //        dataset.push(generatePoint())
+            //    }
+            //}
+            var dataset = generatePoints(250)
+            append(dataset, count)
+            var end = new Date()
+            count += 1
         })
 
     update(dataset)
+    count += 1
     function update(dataset) {
 
         svg.selectAll('circle')
             .data(dataset)
             .enter()
             .append('circle')
-            .attr('r', function (d) {return 10})
+            .attr('r', function (d) {return 3})
             .attr('cx', function (d, i) {
                 return d.x
             })
             .attr('cy', function (d, i) {
                 return d.y
             })
-            .style('fill', 'rgba(255,0,0,0.2)')
+    }
+
+    function append(dataset, count) {
+        svg.selectAll('circle.class' + count)
+            .data(dataset)
+            .enter()
+            .append('circle')
+            .attr('class', 'class' + count)
+            .attr('r', function (d) {return 3})
+            .attr('cx', function (d, i) {
+                return d.x
+            })
+            .attr('cy', function (d, i) {
+                return d.y
+            })
+
     }
 })
